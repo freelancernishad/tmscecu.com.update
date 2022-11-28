aa02da6e1a3cdef292b171606be7fc9f32fe6904
Softlabltd.com <freelancernishad123@gmail.com>
2022-11-28 16:46:20 +0600

    update

--- resources/views/frontlayout.blade.php
+++ resources/views/frontlayout.blade.php
@@ -142,7 +142,7 @@ h5 {
 
 
 
-    <script src="{{ asset('js/frontend.js?ver=1.0.2') }}" async ></script>
+    <script src="{{ asset('js/frontend.js?ver=1.0.3') }}" async ></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
     crossorigin="anonymous"></script>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -167,7 +167,7 @@ li.page-item.active button {
 
 
 
-<script src="{{ asset('js/backend.js?ver=1.0.2') }}"></script>
+<script src="{{ asset('js/backend.js?ver=1.0.3') }}"></script>
 
 
 
