f2d5e4f55031b3b3dd508a009f86e85ef82ff70e
freelancernishad <freelancernishad123@gmail.com>
2023-11-20 20:10:43 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -67,7 +67,7 @@ Route::get('/sent/ekpay/ip', function () {
 
 $curl = curl_init();
 
-$apiUrl = url('get/ekpay/ip');
+$apiUrl = 'https://tepriganjhighschool.edu.bd/get/ekpay/ip';
 curl_setopt_array($curl, array(
   CURLOPT_URL => $apiUrl,
   CURLOPT_RETURNTRANSFER => true,
