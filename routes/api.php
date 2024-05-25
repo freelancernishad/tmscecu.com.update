152b34bb2b0b92941851e550ba8ba3d5b01e976a
freelancernishad <freelancernishad123@gmail.com>
2024-05-25 21:50:01 +0600

    update

--- routes/api.php
+++ routes/api.php
@@ -43,8 +43,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/getip', function (Request $request) {
+
+ return $_SERVER['REMOTE_ADDR'];
 
 
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://tepriganjhighschool.edu.bd/getip.php',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
+
+});
 
 Route::group([
 
@@ -161,7 +184,6 @@ Route::get('/get/form/fillup/students',[studentsController::class , 'formfillups
 
 Route::post('/student/transferto/old',[studentsController::class , 'transertoOld']);
 Route::post('/another/school/data',[studentsController::class , 'addAnotherSchoolData']);
-
 
 
 
