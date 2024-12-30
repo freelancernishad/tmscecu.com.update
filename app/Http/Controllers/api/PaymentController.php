2c504768792ed27561cb17018edd469bc12a3c20
Nishad Hossain <freelancernishad123@gmail.com>
2024-12-30 22:18:14 +0600

    update

--- app/Http/Controllers/api/PaymentController.php
+++ app/Http/Controllers/api/PaymentController.php
@@ -272,9 +272,9 @@ class PaymentController extends Controller
 
         $year = date('Y');
         $yearSession = $student->Year;
-        // if($MonthName=='December'){
-        //     $yearSession = date('Y')+1;
-        // }
+        if($MonthName=='December'){
+            $yearSession = date('Y')+1;
+        }
 
 
 
